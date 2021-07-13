2fb568c848dd5e460cf8852e0a7c157a1bbe97ef
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-13 14:05:42 -0300

    feat: add NetworkGrid component

---
+++ src/components/NetworkGrid/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+
+import { Wrapper } from './styles';
+
+export type Network = {
+  id: string;
+  url: string;
+  image: string;
+  name: string;
+};
+
+interface NetworkGridProps {
+  title: string;
+  data: Network[];
+}
+
+const NetworkGrid: FC<NetworkGridProps> = ({ title, data }) => {
+  return (
+    <Wrapper>
+      <h2 className="smallTitle">
+        {title} ({data.length})
+      </h2>
+      <ul>
+        {data.slice(0, 6).map(element => (
+          <li key={element.id}>
+            <a href={element.url}>
+              <img src={element.image} alt="Profile" />
+              <span>{element.name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </Wrapper>
+  );
+};
+
+export default NetworkGrid;
