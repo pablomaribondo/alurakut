954d372fa892db0f90c7764fce0fe454b1ef7604
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-13 10:47:16 -0300

    refactor: update MainGrid component to use typescript

--- src/components/MainGrid/index.tsx
+++ src/components/MainGrid/index.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const MainGrid = styled.main`
   width: 100%;
@@ -19,7 +19,7 @@ const MainGrid = styled.main`
   @media (min-width: 860px) {
     max-width: 1110px;
     display: grid;
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-areas: 'profileArea welcomeArea profileRelationsArea';
     grid-template-columns: 160px 1fr 312px;
   }
 `;
