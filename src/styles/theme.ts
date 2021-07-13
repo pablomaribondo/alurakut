461800269ed9cc73bf8f1bebde13a63325648f61
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-13 10:41:43 -0300

    feat: add theme colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    backgroundDefault: '#d9e6f6',
+    backgroundSecondary: '#f1f9fe',
+    backgroundTertiary: '#fff',
+    backgroundQuaternary: '#bbcde8',
+    colorPrimary: '#2e7bb4',
+    colorSecondary: '#388bb0',
+    colorTertiary: '#2f4a71',
+    colorQuarternary: '#d81d99',
+    textPrimaryColor: '#333',
+    textSecondaryColor: '#fff',
+    textTertiaryColor: '#5a5a5a',
+    textQuarternaryColor: '#c5c6ca',
+    elementPrimary: '#6f92bb',
+    elementSecondary: '#5579a1',
+    elementTertiary: '#308bc5',
+    elementQuarternary: '#5292c1',
+    divider: '#ecf2fa',
+    gray: '#f4f4f4'
+  },
+  commonRadius: '8px'
+};
+
+export default theme;
